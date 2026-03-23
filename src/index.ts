45051307d87e02638de06710bfba8f70e8d9715a
anjalijangid <anjali.jangid@maitriiinfotech.com>
2026-03-23 16:14:37 +0530

    fix

--- src/index.ts
+++ src/index.ts
@@ -56,7 +56,7 @@ const io = new Server(server, {
 });
 initSocket(io);
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
+const PORT = Number(process.env.PORT) || 5000;
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
